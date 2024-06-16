93e5aaea51dfa408d019920599c2c3450d647137
lhhkms9016 <lhhkms1124@gmail.com>
2024-06-16 23:15:00 +0900

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -4,5 +4,4 @@ st.title("Home Page")
 st.write("Welcome to the Home Page!")
 
 st.page_link("customer1.py", label="Home", icon="🏠")
-st.page_link("pages/page1.py", label="Page 1", icon="1️⃣")
-st.page_link("pages/page2.py", label="회선 트래픽 분석")
+st.page_link("pages/Traffic_회선.py", label="회선 트래픽 분석")
