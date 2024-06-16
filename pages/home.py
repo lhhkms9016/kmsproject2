169c30781ac22ee556c9fbb45b171d1684e499b3
lhhkms9016 <lhhkms1124@gmail.com>
2024-06-16 23:12:12 +0900

    Delete pages directory

--- pages/home.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Home Page")
-st.write("Welcome to the Home Page!")
-
-st.page_link("customer1.py", label="Home", icon="🏠")
-st.page_link("pages/page1.py", label="Page 1", icon="1️⃣")
-st.page_link("pages/page2.py", label="회선 트래픽 분석")
