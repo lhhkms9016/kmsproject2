95d46c013d4da98a7aa03e6d69e4d11aac5fee90
lhhkms9016 <lhhkms1124@gmail.com>
2024-06-17 21:53:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import streamlit as st
 from pages.traffic1 import traffic1
+from pages.traffic2 import app
 
 def main():
 
-    # 세션 상태 초기화
+    # 세션 상태 초기화cd ..
     if 'page' not in st.session_state:
         st.session_state.page = 'Home'
 
     # 사이드바에 입력 위젯 추가
     st.sidebar.title("Menu")
-    page = st.sidebar.selectbox("Select a page", ["Home", "Traffic_1"], index=0)
+    page = st.sidebar.selectbox("Select a page", ["Home", "Traffic_1", "Traffic_2"], index=0)
 
     # 선택된 페이지에 따라 세션 상태 업데이트
     st.session_state.page = page
@@ -19,6 +20,8 @@ def main():
         st.write("여기는 메인 페이지입니다.")
     elif page == "Traffic_1":
         traffic1()
+    elif page == "Traffic_2":
+        app()
 
 if __name__ == '__main__':
     main()
